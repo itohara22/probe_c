237d7c91dea40b18108c6294b561be92d782cd44
ito <ito.k23018@gmail.com>
2026-03-14 17:49:28 +0530

    linked list

--- linked_list.c
+++ linked_list.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 
 
-typedef struct {
+typedef struct node {
     int val;
     struct node* right;
 } node;
@@ -47,6 +47,69 @@ void ll_destroy(linked_list* ll){
     free(ll);
 }
 
+// Returns error codes
+int ll_push_front(linked_list* ll, int val){
+    if (ll == NULL){
+        fprintf(stderr,"Invalid linked list");
+        return 0;
+    }
+
+    node* new_node = malloc(sizeof(node));
+    if(new_node == NULL){
+       fprintf(stderr,"Cannot allocate new node");
+       return 0;
+    }
+
+    new_node->val = val;
+    new_node->right = ll->head;
+    ll->head = new_node;
+    ll->size++;
+    return 1;
+}
+
+int ll_push_back(linked_list* ll, int val){
+    if(ll==NULL){
+        fprintf(stderr,"Invalid linked list");
+        return 0;
+    }
+
+    node* new_node = malloc(sizeof(node));
+    if(new_node == NULL){
+       fprintf(stderr,"Cannot allocate new node");
+       return 0;
+    }
+
+    new_node->val = val;
+    new_node->right = NULL; // need to do this or right will contain garbage
+
+    if(ll->head == NULL){
+       ll->head = new_node;
+       ll->tail = new_node;
+    }else{
+       ll->tail->right = new_node;
+       ll->tail = new_node;
+    }
+    ll->size++;
+    return 1;
+}
 
 
+int ll_pop_front(linked_list* ll, int* val){
+    if(ll==NULL){
+        return 0;
+    }
 
+    if(val == NULL){
+        *val = ll->head->val;
+    }
+
+    node* tmp = ll->head->right;
+    free(ll->head);
+    if(tmp != NULL){
+        ll->head = tmp;
+    }else{
+        ll->tail = NULL;
+    }
+    ll->size--;
+    return 1;
+}
